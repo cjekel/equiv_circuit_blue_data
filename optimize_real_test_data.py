f051e79d12447288893ade8591c5cc98d2564f8b
Charles Jekel <cjekel@gmail.com>
2018-08-05 18:50:08 -0600

    upload opt routine better

--- optimize_real_test_data.py
+++ optimize_real_test_data.py
@@ -323,18 +323,18 @@ def my_opt_fun(obj_function):
     # solver.cr, solver.f = 1.0, 0.9
     best, fit = solver.run(n_it=100)
     fit = fit*-1
-    # # polish with constrained nelder mead simplex optimization
-    # res_cnm = cNM.constrNM(obj_function, best, bounds[:, 0], bounds[:, 1],
-    #                        full_output=True, xtol=1e-15, ftol=1e-15)
-    # # if polish better save polish results
-    # if res_cnm['fopt'] < fit:
-    #     opts = res_cnm['fopt']
-    #     results_x = res_cnm['xopt']
-    #     # print('Polish was better')
-    # else:
-    #     opts = fit
-    #     results_x = best
-    #     # print('Polish did not help')
+    # polish with constrained nelder mead simplex optimization
+    res_cnm = cNM.constrNM(obj_function, best, bounds[:, 0], bounds[:, 1],
+                           full_output=True, xtol=1e-15, ftol=1e-15)
+    # if polish better save polish results
+    if res_cnm['fopt'] < fit:
+        opts = res_cnm['fopt']
+        results_x = res_cnm['xopt']
+        # print('Polish was better')
+    else:
+        opts = fit
+        results_x = best
+        # print('Polish did not help')
 
     # res_bfgs = fmin_l_bfgs_b(obj_function, best, fprime=None, args=(),
     #                          approx_grad=True, bounds=bounds, m=10,
@@ -369,25 +369,25 @@ def my_opt_fun(obj_function):
     # # print(res_ls)
     # # if polish better save polish results
     # # print(fit, res_slsqp[1])
-    # if res_ls.cost < fit:
-    #     opts = res_ls.cost
+    # if 2.0*res_ls.cost < fit:
+    #     opts = res_ls.cost*2.0
     #     results_x = res_ls.x
     #     print('Polish was better')
     # else:
     #     opts = fit
     #     results_x = best
     
-    res_tr = minimize(obj_function, best, method='trust-constr', bounds=bounds, tol=None, callback=None, options={'grad': None, 'xtol': 1e-15, 'gtol': 1e-15, 'barrier_tol': 1e-08, 'sparse_jacobian': None, 'maxiter': 1e10, 'verbose': 0, 'finite_diff_rel_step': None, 'initial_constr_penalty': 1.0, 'initial_tr_radius': 1.0, 'initial_barrier_parameter': 0.1, 'initial_barrier_tolerance': 0.1, 'factorization_method': None, 'disp': False})
-    # print(res_ls)
-    # if polish better save polish results
-    # print(fit, res_slsqp[1])
-    if res_tr.cost < fit:
-        opts = res_tr.cost
-        results_x = res_tr.x
-        print('Polish was better')
-    else:
-        opts = fit
-        results_x = best
+    # res_tr = minimize(obj_function, best, method='trust-constr', bounds=bounds, tol=None, callback=None, options={'grad': None, 'xtol': 1e-15, 'gtol': 1e-15, 'barrier_tol': 1e-08, 'sparse_jacobian': None, 'maxiter': 1e10, 'verbose': 0, 'finite_diff_rel_step': None, 'initial_constr_penalty': 1.0, 'initial_tr_radius': 1.0, 'initial_barrier_parameter': 0.1, 'initial_barrier_tolerance': 0.1, 'factorization_method': None, 'disp': False})
+    # # print(res_ls)
+    # # if polish better save polish results
+    # # print(fit, res_slsqp[1])
+    # if res_tr.cost < fit:
+    #     opts = res_tr.cost
+    #     results_x = res_tr.x
+    #     print('Polish was better')
+    # else:
+    #     opts = fit
+    #     results_x = best
     return np.append(results_x, opts)
 
 
@@ -465,7 +465,6 @@ for ind, data in enumerate(data_list):
         _, _, x_lk, opt_lk = opt_routine(otto_model_L2_Kait)
         # plot_results(x_l1, x_l2, x_linf, x_lk,
         #              'Blue rat ' + data + ' rep ' + str(i+1))
-
         # # save the results into the array
         # res_l1[ind, :5] = x_l1
         # res_l1[ind, 5] = opt_l1
@@ -489,14 +488,15 @@ pd_lk = pd.DataFrame(res_lk, index=index_rows, columns=columns)
 
 # matlab results
 resnorm = np.array([1.1766e+10, 2.6300e+09, 7.5815e+11, 7.5814e+11,
-                    1.8967e+14, 3.2436e+14, 2.0287e+15, 1.4001e+15,
+                    1.8967e+14, 3.2436e+14, 2.1614e+15, 1.4802e+15,
                     1.7229e+11, 4.6677e+11])
 
-results = np.zeros((10, 3))
+results = np.zeros((10, 4))
 results[:, 0] = resnorm
 results[:, 1] = res_lk[:, 5]
 results[:, 2] = results[:, 0] - results[:, 1]
+results[:, 3] = (results[:, 2] / results[:, 1])*100.
 colz = ['MATLAB lsqnonlin', 'JADE DE',
-        'MATLAB - JADE (positive if JADE better)']
+        'MATLAB - JADE', 'Percent difference']
 pd_compare = pd.DataFrame(results, index=index_rows, columns=colz)
 print(pd_compare)
